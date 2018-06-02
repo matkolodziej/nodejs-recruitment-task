7f57379920eec2addee70ff1dcfc91c1ab3e439e
Mat Kolodziej <mat.kolodziej@hotmail.com>
2018-06-02 09:43:04 -0600

    fixed confi

--- config/config.js
+++ config/config.js
@@ -6,6 +6,8 @@ if (env === "development") {
 } else if (env === "test") {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = "mongodb://localhost/nodejs-recruitment-test";
-} else if(env === "heroku") {
-  
+} else if (env === "production") {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI =
+    "mongodb://heroku_gsx6pcdh:5mcipmu29nev70q73b5n7n4s25@ds253879.mlab.com:53879/heroku_gsx6pcdh";
 }
