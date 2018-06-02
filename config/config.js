d08e04d6cc4d831200b1b393943b6a51c2908532
Mat Kolodziej <mat.kolodziej@hotmail.com>
2018-06-02 09:59:09 -0600

    another config.js fix

--- config/config.js
+++ config/config.js
@@ -7,7 +7,6 @@ if (env === "development") {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = "mongodb://localhost/nodejs-recruitment-test";
 } else if (env === "production") {
-  process.env.PORT = 3000;
   process.env.MONGODB_URI =
     "mongodb://heroku_gsx6pcdh:5mcipmu29nev70q73b5n7n4s25@ds253879.mlab.com:53879/heroku_gsx6pcdh";
 }
